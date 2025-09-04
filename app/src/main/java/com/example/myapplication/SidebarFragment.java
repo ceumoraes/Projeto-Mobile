f4eb08a9efe82969b597081f9ac8bd76db5790ea
Celeste Moraes <celestemoraes1706@gmail.com>
2025-09-04 18:14:13 -0300

    Add social media link handling to SidebarFragment
    Added click listeners for Behance, Instagram, YouTube, and Bluesky logos in SidebarFragment to open respective URLs. Also updated ContatoActivity in AndroidManifest to adjust window soft input mode for better keyboard handling.

--- app/src/main/java/com/example/myapplication/SidebarFragment.java
+++ app/src/main/java/com/example/myapplication/SidebarFragment.java
@@ -1,11 +1,13 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SidebarFragment extends Fragment {
@@ -22,6 +24,11 @@ public class SidebarFragment extends Fragment {
         TextView trabalhos = view.findViewById(R.id.textView3);
         TextView selos = view.findViewById(R.id.textView4);
         TextView contato = view.findViewById(R.id.textView5);
+
+        ImageView behanceLogo = view.findViewById(R.id.imageView3);
+        ImageView InstagramLogo = view.findViewById(R.id.imageView5);
+        ImageView YoutubeLogo = view.findViewById(R.id.imageView6);
+        ImageView BlueskyLogo = view.findViewById(R.id.imageView4);
 
         trabalhos.setOnClickListener(v -> {
             Intent intent = new Intent(requireActivity(), MainActivity.class);
@@ -41,6 +48,24 @@ public class SidebarFragment extends Fragment {
             requireActivity().finish();
         });
 
+        behanceLogo.setOnClickListener(v -> {
+            abrirLink("https://www.behance.net/kadeajojo");
+        });
+        InstagramLogo.setOnClickListener(v -> {
+            abrirLink("https://www.instagram.com/kadeajojo/");
+        });
+        YoutubeLogo.setOnClickListener(v -> {
+            abrirLink("https://www.youtube.com/@jojoslullaby");
+        });
+        BlueskyLogo.setOnClickListener(v -> {
+            abrirLink("https://bsky.app/profile/kadeajojo.bsky.social");
+        });
+
         return view;
+    }
+
+    private void abrirLink(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
